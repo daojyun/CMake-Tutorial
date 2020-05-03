4e31a179e5165568b62759bcf5e551297721c2d5
彭呆瓜 <daojyun@gmail.com>
2020-05-03 15:36:55 +0800

    save Step4

---
+++ Step4/MathFunctions/mysqrt.cxx
@@ -0,0 +1,10 @@
+
+#include <cmath>
+#include <iostream>
+
+#include "MathFunctions.h"
+
+double mysqrt(double value) {
+  std::cout << "[mysqrt:" << value << "]" << std::endl;
+  return std::sqrt(value);
+}
